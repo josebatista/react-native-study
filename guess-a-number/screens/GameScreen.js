afbdf59c760fd7cec3f7057d2753960f0c1ec1e7
jpereira <josebatistapereira@gmail.com>
2021-02-01 20:01:12 -0300

    using Dimensions in 'if' checks

--- guess-a-number/screens/GameScreen.js
+++ guess-a-number/screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { StyleSheet, View, Alert, FlatList } from 'react-native'
+import { StyleSheet, View, Alert, FlatList, Dimensions } from 'react-native'
 import { Foundation } from '@expo/vector-icons'
 
 import BodyText from '../components/BodyText'
@@ -67,6 +67,12 @@ const GameScreen = props => {
         setPastGuesses(currentPastGuesses => [nextNumber.toString(), ...currentPastGuesses])
     }
 
+    let listContainerStyle = styles.listContainer
+
+    if (Dimensions.get('window').width < 350) {
+        listContainerStyle = styles.listContainerBig
+    }
+
     return (
         <View style={styles.screen}>
             <TitleText>Opponent's Guess</TitleText>
@@ -79,7 +85,7 @@ const GameScreen = props => {
                     <Foundation name="plus" size={24} color="white" />
                 </MainButton>
             </Card>
-            <View style={styles.listContainer}>
+            <View style={listContainerStyle}>
                 {/* <ScrollView contentContainerStyle={styles.list}>
                     {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
                 </ScrollView> */}
@@ -103,13 +109,17 @@ const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
-        marginTop: 20,
+        marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
         width: 400,
         maxWidth: '90%'
     },
     listContainer: {
         flex: 1,
         width: '60%'
+    },
+    listContainerBig: {
+        flex: 1,
+        width: '80%'
     },
     list: {
         flexGrow: 1,
