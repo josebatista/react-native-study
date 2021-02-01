977eca105a9ff1fd38b29e8639117fc4c796347f
jpereira <josebatistapereira@gmail.com>
2021-02-01 12:39:10 -0300

    styling result text

--- guess-a-number/screens/GameOverScreen.js
+++ guess-a-number/screens/GameOverScreen.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { StyleSheet, View, Button, Image } from 'react-native'
+import { StyleSheet, View, Text, Button, Image } from 'react-native'
 
 import BodyText from '../components/BodyText'
 import TitleText from '../components/TitleText'
+
+import Colors from '../constants/colors'
 
 const GameOverScreen = props => {
     return (
@@ -16,8 +18,14 @@ const GameOverScreen = props => {
                     resizeMode="cover"
                 />
             </View>
-            <BodyText>Number of rounds: {props.numberOfRounds}</BodyText>
-            <BodyText>Number was: {props.userNumber}</BodyText>
+            <View style={styles.resultContainer}>
+                <BodyText style={styles.resultText}>
+                    Your phone needed{' '}
+                    <Text style={styles.highlight}>{props.numberOfRounds}</Text>{' '}
+                 rounds to guess the number{' '}
+                    <Text style={styles.highlight}>{props.userNumber}</Text>.
+            </BodyText>
+            </View>
             <Button title="New Game" onPress={props.onRestart} />
         </View>
     )
@@ -41,6 +49,18 @@ const styles = StyleSheet.create({
     image: {
         width: '100%',
         height: '100%'
+    },
+    resultContainer: {
+        marginHorizontal: 30,
+        marginVertical: 15
+    },
+    resultText: {
+        textAlign: 'center',
+        fontSize: 20
+    },
+    highlight: {
+        color: Colors.primary,
+        fontFamily: 'open-sans-bold'
     }
 })
 
