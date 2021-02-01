88768371e960fc6a47f5a80483c1c9485030ca9f
jpereira <josebatistapereira@gmail.com>
2021-01-31 22:36:31 -0300

    adding custom fonts

--- guess-a-number/App.js
+++ guess-a-number/App.js
@@ -1,3 +1,5 @@
+import * as Font from 'expo-font'
+import AppLoading from 'expo-app-loading'
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
@@ -7,10 +9,26 @@ import GameOverScreen from './screens/GameOverScreen';
 import GameScreen from './screens/GameScreen';
 import StartGameScreen from './screens/StartGameScreen';
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
+
 export default function App() {
 
   const [userNumber, setUserNumber] = useState()
   const [guessRounds, setGuessRounds] = useState(0)
+  const [dataLoaded, setDataLoaded] = useState(false)
+
+  if (!dataLoaded) {
+    return <AppLoading
+      startAsync={fetchFonts}
+      onFinish={() => setDataLoaded(true)}
+      onError={err => console.error(err)}
+    />
+  }
 
   const configureNewGameHandler = () => {
     setGuessRounds(0)
