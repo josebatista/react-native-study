1e95739acc426659fe21c88439ce1eed9494ccf6
jpereira <josebatistapereira@gmail.com>
2021-02-01 14:25:13 -0300

    scrollview and flexbox

--- guess-a-number/screens/GameScreen.js
+++ guess-a-number/screens/GameScreen.js
@@ -79,8 +79,8 @@ const GameScreen = props => {
                     <Foundation name="plus" size={24} color="white" />
                 </MainButton>
             </Card>
-            <View style={styles.list}>
-                <ScrollView>
+            <View style={styles.listContainer}>
+                <ScrollView contentContainerStyle={styles.list}>
                     {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
                 </ScrollView>
             </View>
@@ -101,9 +101,14 @@ const styles = StyleSheet.create({
         width: 400,
         maxWidth: '90%'
     },
-    list: {
+    listContainer: {
         flex: 1,
         width: '80%'
+    },
+    list: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-end'
     },
     listItem: {
         borderColor: '#ccc',
@@ -112,7 +117,8 @@ const styles = StyleSheet.create({
         marginVertical: 10,
         backgroundColor: 'white',
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        width: '60%'
     }
 })
 
