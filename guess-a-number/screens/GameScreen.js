9329b7296d2232108e8966fa3948a999d80f880d
jpereira <josebatistapereira@gmail.com>
2021-01-31 21:47:21 -0300

    checking the 'win' condition with useEffect hook

--- guess-a-number/screens/GameScreen.js
+++ guess-a-number/screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { StyleSheet, View, Text, Button, Alert } from 'react-native'
 
 import Card from '../components/Card'
@@ -18,10 +18,22 @@ const generateRandomBetween = (min, max, exclude) => {
 
 const GameScreen = props => {
 
-    const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice))
+    const [currentGuess, setCurrentGuess] = useState(
+        generateRandomBetween(1, 100, props.userChoice)
+    )
+
+    const [rounds, setRounds] = useState(0)
 
     const currentLow = useRef(1)
     const currentHigh = useRef(100)
+
+    const { userChoice, onGameOver } = props
+
+    useEffect(() => {
+        if (currentGuess === props.userChoice) {
+            props.onGameOver(rounds)
+        }
+    }, [currentGuess, userChoice, onGameOver])
 
     const nextGuessHandler = direction => {
         if (
@@ -41,6 +53,7 @@ const GameScreen = props => {
 
         const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess)
         setCurrentGuess(nextNumber)
+        setRounds(currentRounds => currentRounds + 1)
     }
 
     return (
