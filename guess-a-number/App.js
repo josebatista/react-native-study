60f600bcaf327300d59393d5ebfcd528e8876757
jpereira <josebatistapereira@gmail.com>
2021-01-31 21:57:06 -0300

    Finishing the game logic

--- guess-a-number/App.js
+++ guess-a-number/App.js
@@ -12,9 +12,13 @@ export default function App() {
   const [userNumber, setUserNumber] = useState()
   const [guessRounds, setGuessRounds] = useState(0)
 
+  const configureNewGameHandler = () => {
+    setGuessRounds(0)
+    setUserNumber(null)
+  }
+
   const startGameHandler = selectedNumber => {
     setUserNumber(selectedNumber)
-    setGuessRounds(0)
   }
 
   const gameOverHandler = numOfRound => {
@@ -26,7 +30,7 @@ export default function App() {
   if (userNumber && guessRounds <= 0) {
     content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
   } else if (guessRounds > 0) {
-    content = <GameOverScreen />
+    content = <GameOverScreen userNumber={userNumber} numberOfRounds={guessRounds} onRestart={configureNewGameHandler} />
   }
 
   return (
--- guess-a-number/screens/GameOverScreen.js
+++ guess-a-number/screens/GameOverScreen.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Button } from 'react-native'
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
             <Text>The game is Over!</Text>
+            <Text>Number of rounds: {props.numberOfRounds}</Text>
+            <Text>Number was: {props.userNumber}</Text>
+            <Button title="New Game" onPress={props.onRestart} />
         </View>
     )
 }
