babca356ea54b3c6a82f0e277084c974d9e490c9
jpereira <josebatistapereira@gmail.com>
2021-02-01 14:17:21 -0300

    Styling list and list items

--- guess-a-number/screens/GameScreen.js
+++ guess-a-number/screens/GameScreen.js
@@ -1,7 +1,8 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { StyleSheet, View, Text, Alert, ScrollView } from 'react-native'
+import { StyleSheet, View, Alert, ScrollView } from 'react-native'
 import { Foundation } from '@expo/vector-icons'
 
+import BodyText from '../components/BodyText'
 import Card from '../components/Card'
 import MainButton from '../components/MainButton'
 import NumberContainer from '../components/NumberContainer'
@@ -18,6 +19,13 @@ const generateRandomBetween = (min, max, exclude) => {
 
     return rndNum
 }
+
+const renderListItem = (value, numOfRound) => (
+    <View key={value} style={styles.listItem}>
+        <BodyText>#{numOfRound}</BodyText>
+        <BodyText>{value}</BodyText>
+    </View>
+)
 
 const GameScreen = props => {
 
@@ -71,13 +79,11 @@ const GameScreen = props => {
                     <Foundation name="plus" size={24} color="white" />
                 </MainButton>
             </Card>
-            <ScrollView>
-                {pastGuesses.map(guess => (
-                    <View key={guess}>
-                        <Text>{guess}</Text>
-                    </View>
-                ))}
-            </ScrollView>
+            <View style={styles.list}>
+                <ScrollView>
+                    {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
+                </ScrollView>
+            </View>
         </View>
     )
 }
@@ -94,6 +100,19 @@ const styles = StyleSheet.create({
         marginTop: 20,
         width: 400,
         maxWidth: '90%'
+    },
+    list: {
+        flex: 1,
+        width: '80%'
+    },
+    listItem: {
+        borderColor: '#ccc',
+        borderWidth: 1,
+        padding: 15,
+        marginVertical: 10,
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     }
 })
 
