fa849227712a08a97ca49b43e968a449105724cf
jpereira <josebatistapereira@gmail.com>
2021-03-01 23:47:46 -0300

    adding a menu button and drawer navigation

--- the_meals_app/screens/FavoritesScreen.js
+++ the_meals_app/screens/FavoritesScreen.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 
 import { MEALS } from '../data/dummy-data'
 
 import MealList from '../components/MealList'
+import HeaderButton from '../components/HeaderButton'
 
 const FavoritesScreen = props => {
 
@@ -11,8 +13,15 @@ const FavoritesScreen = props => {
     return <MealList listData={favMeals} navigation={props.navigation} />
 }
 
-FavoritesScreen.navigationOptions = {
-    headerTitle: 'Your Favorites'
+FavoritesScreen.navigationOptions = navData => {
+    return {
+        headerTitle: 'Your Favorites',
+        headerLeft: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
+            <Item title='Menu' iconName='ios-menu' onPress={() => {
+                navData.navigation.toggleDrawer()
+            }} />
+        </HeaderButtons>
+    }
 }
 
 
