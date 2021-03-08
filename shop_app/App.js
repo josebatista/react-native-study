2ce63ade777d1c5015dde97f9f20160494bdba45
jpereira <josebatistapereira@gmail.com>
2021-03-07 22:20:58 -0300

    Using custom fonts

--- shop_app/App.js
+++ shop_app/App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import AppLoading from 'expo-app-loading'
+import * as Font from 'expo-font'
+
+import React, { useState } from 'react';
 
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
@@ -14,7 +17,25 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer)
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
+
 export default function App() {
+
+  const [fontLoaded, setFontLoaded] = useState(false)
+
+  if (!fontLoaded) {
+    return <AppLoading
+      startAsync={fetchFonts}
+      onFinish={() => setFontLoaded(true)}
+      onError={(err) => console.error(err)}
+    />
+  }
+
   return (
     <Provider store={store}>
       <StatusBar style="auto" />
