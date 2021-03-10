2cfadd117a72be90b42680a242490324a33bb184
jpereira <josebatistapereira@gmail.com>
2021-03-10 20:45:33 -0300

    clearing the cart

--- shop_app/store/reducers/cart.js
+++ shop_app/store/reducers/cart.js
@@ -1,6 +1,7 @@
 import CartItem from '../../models/cart-item'
 
 import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/cart"
+import { ADD_ORDER } from '../actions/orders'
 
 const initialState = {
     items: {},
@@ -55,6 +56,8 @@ export default (state = initialState, action) => {
                 items: updatedCartItems,
                 totalAmount: state.totalAmount - selectedCartItem.productPrice
             }
+        case ADD_ORDER:
+            return initialState
     }
     return state
 }
