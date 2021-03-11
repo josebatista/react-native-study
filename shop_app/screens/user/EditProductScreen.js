e7aecb5e054531ed3c17c68c366fda2112926f20
jpereira <josebatistapereira@gmail.com>
2021-03-10 22:55:58 -0300

    using params to submit user input

--- shop_app/screens/user/EditProductScreen.js
+++ shop_app/screens/user/EditProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { View, ScrollView, Text, TextInput, StyleSheet, Platform } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import { useSelector } from 'react-redux'
@@ -16,6 +16,14 @@ const EditProductScreen = props => {
     const [imageUrl, setImageUrl] = useState(editedProduct ? editedProduct.imageUrl : '')
     const [price, setPrice] = useState('')
     const [description, setDescription] = useState(editedProduct ? editedProduct.description : '')
+
+    const submitHandler = useCallback(() => {
+        console.log('submiting!!!')
+    }, [])
+
+    useEffect(() => {
+        props.navigation.setParams({ submit: submitHandler })
+    }, submitHandler)
 
     return (
         <ScrollView>
@@ -44,15 +52,14 @@ const EditProductScreen = props => {
 }
 
 EditProductScreen.navigationOptions = navData => {
+    const submitFn = navData.navigation.getParam('submit')
     return {
         headerTitle: navData.navigation.getParam('productId') ? 'Edit Product' : 'Add Product',
         headerRight: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
             <Item
                 title='Save'
                 iconName={Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'}
-                onPress={() => {
-                    navData.navigation.navigate('EditProduct')
-                }}
+                onPress={submitFn}
             />
         </HeaderButtons>
     }
