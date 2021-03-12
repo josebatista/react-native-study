64ae3b01945316c7abd51071ebdccd82acabc686
jpereira <josebatistapereira@gmail.com>
2021-03-11 21:43:54 -0300

    configuring TextInputs

--- shop_app/screens/user/EditProductScreen.js
+++ shop_app/screens/user/EditProductScreen.js
@@ -39,7 +39,17 @@ const EditProductScreen = props => {
             <View style={styles.form}>
                 <View style={styles.formControl}>
                     <Text style={styles.label}>Title</Text>
-                    <TextInput style={styles.input} value={title} onChangeText={text => setTitle(text)} />
+                    <TextInput
+                        style={styles.input}
+                        value={title}
+                        onChangeText={text => setTitle(text)}
+                        keyboardType='default'
+                        autoCapitalize='sentences'
+                        autoCorrect
+                        returnKeyType='next'
+                        onEndEditing={() => console.log('onEndEditing')}
+                        onSubmitEditing={() => console.log('onSubmitEditing')}
+                    />
                 </View>
                 <View style={styles.formControl}>
                     <Text style={styles.label}>Image Url</Text>
@@ -48,7 +58,12 @@ const EditProductScreen = props => {
                 {editedProduct ? null : (
                     <View style={styles.formControl}>
                         <Text style={styles.label}>Price</Text>
-                        <TextInput style={styles.input} value={price} onChangeText={text => setPrice(text)} />
+                        <TextInput
+                            style={styles.input}
+                            value={price}
+                            onChangeText={text => setPrice(text)}
+                            keyboardType='decimal-pad'
+                        />
                     </View>
                 )}
                 <View style={styles.formControl}>
