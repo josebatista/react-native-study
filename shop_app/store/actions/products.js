dec73038f3b299ad137d25b5c2d46703a53c5356
jpereira <josebatistapereira@gmail.com>
2021-03-19 10:06:17 -0300

    storing products on a server

--- shop_app/store/actions/products.js
+++ shop_app/store/actions/products.js
@@ -2,19 +2,40 @@ export const DELETE_PRODUCT = 'DELETE_PRODUCT'
 export const CREATE_PRODUCT = 'CREATE_PRODUCT'
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT'
 
+const BASE_URL = 'https://shopapp-25344-default-rtdb.firebaseio.com/products.json'
+
 export const deleteProduct = productId => {
     return { type: DELETE_PRODUCT, pid: productId }
 }
 
 export const createProduct = (title, description, imageUrl, price) => {
-    return {
-        type: CREATE_PRODUCT,
-        productData: {
-            title,
-            description,
-            imageUrl,
-            price
-        }
+    return async dispatch => {
+        //any async code you want!
+        const response = await fetch(BASE_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                description,
+                imageUrl,
+                price
+            })
+        })
+
+        const resData = await response.json()
+
+        dispatch({
+            type: CREATE_PRODUCT,
+            productData: {
+                id: resData.name,
+                title,
+                description,
+                imageUrl,
+                price
+            }
+        })
     }
 }
 
