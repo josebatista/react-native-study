2fde5e29270085913d2cd5e3be634acd582129e0
jpereira <josebatistapereira@gmail.com>
2021-03-21 11:08:00 -0300

    storing orders

--- shop_app/store/actions/orders.js
+++ shop_app/store/actions/orders.js
@@ -1,5 +1,30 @@
 export const ADD_ORDER = 'ADD_ORDER'
 
+import BASE_URL from '../../constants/BaseURL'
+
 export const addOrder = (cartItems, totalAmount) => {
-    return { type: ADD_ORDER, orderData: { items: cartItems, amount: totalAmount } }
+    return async dispatch => {
+
+        const date = new Date()
+
+        const response = await fetch(`${BASE_URL}/orders/u1.json`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                cartItems,
+                totalAmount,
+                date: date.toISOString()
+            })
+        })
+
+        if (!response.ok) {
+            throw Error('Something went wrong!!')
+        }
+
+        const resData = await response.json()
+
+        dispatch({ type: ADD_ORDER, orderData: { id: resData.name, items: cartItems, amount: totalAmount, date: date } })
+    }
 }
