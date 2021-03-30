eba2db834be05fbcdc1479c9641a440cd631e8a0
jpereira <josebatistapereira@gmail.com>
2021-03-30 15:12:20 -0300

    Screen and Navigation setup

---
+++ great_places/screens/NewPlaceScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const NewPlaceScreen = () => {
+    return (
+        <View>
+            <Text>NewPlaceScreen</Text>
+        </View>
+    )
+}
+
+NewPlaceScreen.navigationOptions = {
+    headerTitle: 'Add Place'
+}
+
+const styles = StyleSheet.create({})
+
+export default NewPlaceScreen
---
+++ great_places/screens/PlaceDetailScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const PlaceDetailScreen = () => {
+    return (
+        <View>
+            <Text>PlaceDetailScreen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default PlaceDetailScreen
