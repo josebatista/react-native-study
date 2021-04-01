12efb56d02b77b5ccb79454d6fcdca7587d42474
jpereira <josebatistapereira@gmail.com>
2021-04-01 13:47:05 -0300

    Diving into SQLite for permanent data storage

--- great_places/App.js
+++ great_places/App.js
@@ -3,10 +3,20 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import ReduxThunk from 'redux-thunk'
 
+import { init } from './helpers/db'
+
 import PlacesReducer from './store/places-reducer'
 
 import PlacesNavigator from './navigation/PlacesNavigator'
 
+init()
+  .then(() => {
+    console.log('Initialized database')
+  })
+  .catch((err) => {
+    console.log('Initializing db falied')
+    console.error(err)
+  })
 
 const rootReducer = combineReducers({
   places: PlacesReducer
