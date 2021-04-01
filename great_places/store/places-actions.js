cca300b8ee6d6628eef94d48c3ffeca2b1e49672
jpereira <josebatistapereira@gmail.com>
2021-04-01 14:15:36 -0300

    Fetching data from the local database

--- great_places/store/places-actions.js
+++ great_places/store/places-actions.js
@@ -1,8 +1,9 @@
 import * as FileSystem from 'expo-file-system'
 
-import { insertPlace } from '../helpers/db'
+import { fetchPlaces, insertPlace } from '../helpers/db'
 
 export const ADD_PLACE = 'ADD_PLACE'
+export const SET_PLACES = 'SET_PLACES'
 
 export const addPlace = (title, image) => {
     return async dispatch => {
@@ -23,11 +24,21 @@ export const addPlace = (title, image) => {
                 12.3
             )
 
-            console.log(dbResult)
-
             dispatch({ type: ADD_PLACE, placeData: { id: dbResult.insertId, title: title, image: newPath } })
         } catch (err) {
             console.error(err)
+            throw err
+        }
+    }
+}
+
+export const loadPlaces = () => {
+    return async dispatch => {
+        try {
+            const dbResult = await fetchPlaces()
+
+            dispatch({ type: SET_PLACES, places: dbResult.rows._array })
+        } catch (err) {
             throw err
         }
     }
