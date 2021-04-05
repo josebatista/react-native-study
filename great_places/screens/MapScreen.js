61251abe95d871af3564ad8da9aa870f8f69308c
jpereira <josebatistapereira@gmail.com>
2021-04-05 09:37:55 -0300

    Making the picked location 'saveable'

--- great_places/screens/MapScreen.js
+++ great_places/screens/MapScreen.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
-import { StyleSheet } from 'react-native'
+import React, { useState, useEffect, useCallback } from 'react'
+import { Platform, StyleSheet, Text, TouchableOpacity } from 'react-native'
 import MapView, { Marker } from 'react-native-maps'
 
-const MapScreen = () => {
+import Colors from '../constants/Colors'
+
+const MapScreen = props => {
 
     const [selectedLocation, setSelectedLocation] = useState()
 
@@ -19,6 +21,18 @@ const MapScreen = () => {
             lng: event.nativeEvent.coordinate.longitude
         })
     }
+
+    const savePickedLocationHandler = useCallback(() => {
+        if (!selectedLocation) {
+            //could show an alert
+            return
+        }
+        props.navigation.navigate('NewPlace', { pickedLocation: selectedLocation })
+    }, [selectedLocation])
+
+    useEffect(() => {
+        props.navigation.setParams({ saveLocation: savePickedLocationHandler })
+    }, [savePickedLocationHandler])
 
     let markerCoordinates
 
@@ -38,9 +52,27 @@ const MapScreen = () => {
     )
 }
 
+MapScreen.navigationOptions = navData => {
+
+    const saveFn = navData.navigation.getParam('saveLocation')
+
+    return {
+        headerRight: <TouchableOpacity style={styles.headerButton} onPress={saveFn}>
+            <Text style={styles.headerButtonText}>Save</Text>
+        </TouchableOpacity>
+    }
+}
+
 const styles = StyleSheet.create({
     map: {
         flex: 1
+    },
+    headerButton: {
+        marginHorizontal: 20
+    },
+    headerButtonText: {
+        fontSize: 16,
+        color: Platform.OS === 'android' ? 'white' : Colors.primary
     }
 })
 
